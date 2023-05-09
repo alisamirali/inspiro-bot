fbdfd64017ad6c10b8c153c28f813ced5363dd03
Ali Samir <alisamir.eng@gmail.com>
2023-05-10 01:10:03 +0300

    Deploy the app

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "InspiroBot",
+  description: "Discover & Share AI Prompts",
+};
+
+const Layout = ({ children }) => (
+  <html lang="en">
+    <head>
+      <link rel="icon" href="assets/images/logo.svg" />
+    </head>
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default Layout;
